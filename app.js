2b3b7c509aa8cce0f849afda9e5ff6e898489db3
CP <chandrapratap321@gmail.com>
2021-03-09 00:06:37 +0530

    corrects the listening port

--- app.js
+++ app.js
@@ -4,4 +4,6 @@ app.get("/", (req, res, next) => {
   res.json({ working: true });
 });
 
-app.listen(3000, () => console.log("App is listening on port 3000"));
+app.listen(process.env?.PORT || 3000, () =>
+  console.log("App is listening on port 3000")
+);
